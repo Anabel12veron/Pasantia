59ba06c1383d17252bd11ce61fe3c1f7e65face8
Anabel12veron <ana15bel.veron@gmail.com>
2022-05-16 08:25:54 -0300

    direcciones corregidas para el uso del proyecto sin internet

--- pantallas/comentario.php
+++ pantallas/comentario.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comentarios</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap.min.css">
     <link rel="stylesheet" href="../assets/estiloInicio.css">
 </head>
 
@@ -49,8 +49,8 @@
 
 
 <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="../jquery/3.3.1/jquery.min.js"></script>
+    <script src="../bootstrap.bundle.min.js"></script>
  
 	<script src="../assets/script.js"></script>
 	</div>
